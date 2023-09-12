3461f5ae4194c1795611d35a0f96ff736e56e1a7
kirankumarhm <kirankumarhm2004@gmail.com>
2023-09-12 15:01:23 +0530

    Product Service CQRS - Validation if record alredy exists

--- product-service/src/main/java/com/microservice/product/service/controller/CreateProductCommandInterceptor.java
+++ product-service/src/main/java/com/microservice/product/service/controller/CreateProductCommandInterceptor.java
@@ -1,6 +1,9 @@
 package com.microservice.product.service.controller;
 
 import com.microservice.product.service.command.CreateProductCommand;
+import com.microservice.product.service.model.Product;
+import com.microservice.product.service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.messaging.MessageDispatchInterceptor;
@@ -13,8 +16,10 @@ import java.util.function.BiFunction;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
 
+    private final ProductRepository productRepository;
 
     @Nonnull
     @Override
@@ -22,16 +27,29 @@ public class CreateProductCommandInterceptor implements MessageDispatchIntercept
 
 
         return (index, command) -> {
-            log.info("Intercepted command : {}",command.getPayload());
+            log.info("Intercepted command : {}",command.getPayloadType());
 
-            if(CreateProductCommand.class.equals(command.getPayload())) {
+            if(CreateProductCommand.class.equals(command.getPayloadType())) {
+
                 CreateProductCommand productCommand = (CreateProductCommand) command.getPayload();
-                if(productCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0){
-                    throw new IllegalArgumentException("Price can not be less than or equal to zero.");
+
+//                if(productCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0){
+//                    throw new IllegalArgumentException("Price can not be less than or equal to zero.");
+//                }
+//                if(productCommand.getTitle() == null || productCommand.getTitle().isBlank()){
+//                    throw new IllegalArgumentException("Title can not be empty.");
+//                }
+
+                Product product = productRepository.findByProductIdOrTitle(productCommand.getProductId(), productCommand.getTitle());
+
+                log.info("product : {}",product);
+
+                if(product != null){
+                    throw new IllegalStateException(String.format(
+                            "Product with productId %s or title %s already exists"
+                            ,productCommand.getProductId(), productCommand.getTitle()));
                 }
-                if(productCommand.getTitle() == null || productCommand.getTitle().isBlank()){
-                    throw new IllegalArgumentException("Title can not be empty.");
-                }
+
             }
             return command;
         };
