e36ad2760a08300c6c4ca32bb1ad121acfbbe7ee
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2023-09-29 17:54:31 +0530

    1. Made containerization and created docker-compose.yml 2. Set all the environment properties in the docker-compose environment

--- payments-service/src/main/java/com/microservice/payment/example/PaymentsServiceApplication.java
+++ payments-service/src/main/java/com/microservice/payment/example/PaymentsServiceApplication.java
@@ -3,9 +3,11 @@ package com.microservice.payment.example;
 import com.microservice.payment.example.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import({ AxonConfig.class })
 
 public class PaymentsServiceApplication {
